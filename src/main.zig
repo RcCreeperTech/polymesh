61b92e9151d7497137fba4e59412c2158a031476
RcCreeperTech <devinprothero@gmail.com>
2024-09-05 20:16:32 -0700

    Dramatically reduce complexity of arc insertion and index selection

--- src/main.zig
+++ src/main.zig
@@ -98,34 +98,24 @@ const Voronoi = struct {
             f32, // yval
         } {
             log.info("Searcing Point {{{d}, {d}}}", .{ point.x, point.y });
-            // Boundary Conditions
             if (self.arcs.items.len == 0) return .{ 0, 0 };
 
-            const index_of_first_arc_greater_than_or_equal_to_point = std.sort.lowerBound(Arc, self.arcs.items, point.x, Arc.compare);
-            if (index_of_first_arc_greater_than_or_equal_to_point == 0) {
-                log.info("Hit lower boundary", .{});
-                const arc = self.getArc(0);
-                const y = arc.evaluate(point.x, point.y);
-                return .{ 0, y };
-            } else if (index_of_first_arc_greater_than_or_equal_to_point == self.arcs.items.len) {
-                log.info("Hit upper boundary", .{});
-                const arc = self.arcs.getLast();
-                const y = arc.evaluate(point.x, point.y);
-                return .{ self.arcs.items.len - 1, y };
-            } else {
-                log.info("Hit middle boundary", .{});
-                const lower_idx = index_of_first_arc_greater_than_or_equal_to_point - 1;
-                const higher_idx = index_of_first_arc_greater_than_or_equal_to_point;
+            const higher_idx = std.sort.lowerBound(Arc, self.arcs.items, point.x, Arc.compare);
+            const lower_idx = higher_idx -| 1;
 
-                const ylow = self.getArc(lower_idx).evaluate(point.x, point.y);
+            const lower_arc = self.getArc(lower_idx);
+            const ylow = lower_arc.evaluate(point.x, point.y);
+
+            if (higher_idx != 0 and higher_idx != self.arcs.items.len) {
                 const yhigh = self.getArc(higher_idx).evaluate(point.x, point.y);
-
                 if (yhigh > ylow) {
                     return .{ higher_idx, yhigh };
                 } else {
                     return .{ lower_idx, ylow };
                 }
             }
+
+            return .{ lower_idx, ylow };
         }
 
         pub fn getArc(self: *const @This(), index: usize) Arc {
@@ -168,7 +158,7 @@ const Voronoi = struct {
                 cancelled: bool = false,
                 center: rl.Vector2,
                 radius: f32,
-                arc: *Beachline.Arc,
+                arc_idx: usize,
             },
 
             pub fn sortHandle(self: *const @This()) f32 {
@@ -231,7 +221,7 @@ const Voronoi = struct {
 
             try self.queue.ensureUnusedCapacity(1);
 
-            const start_index = self.queue.items.len - 1;
+            const start_index = self.queue.items.len -| 1;
             self.queue.items.len += 1;
             self.queue.items[start_index] = e;
 
@@ -333,19 +323,9 @@ const Voronoi = struct {
         if (beach_items.len < 3) return;
 
         const arc = self.beachline.getArcPtr(arc_idx);
+        if (arc.boundary_left == null or arc.boundary_right == null) return;
 
-        const lo, const hi = get_focii_range: {
-            if (arc.boundary_left == null) {
-                log.info("Hit Start Cond", .{});
-                break :get_focii_range .{ arc_idx, 3 };
-            } else if (arc.boundary_right == null) {
-                log.info("Hit End Cond", .{});
-                break :get_focii_range .{ arc_idx - 2, beach_items.len };
-            } else {
-                log.info("Hit Middle Cond", .{});
-                break :get_focii_range .{ arc_idx - 1, arc_idx + 2 };
-            }
-        };
+        const lo, const hi = .{ arc_idx - 1, arc_idx + 2 };
         log.info("Range: {d}..{d}", .{ lo, hi });
         const focii = beach_items[lo..hi];
 
@@ -354,7 +334,7 @@ const Voronoi = struct {
                 .circle = .{
                     .center = circle.center,
                     .radius = circle.radius,
-                    .arc = arc,
+                    .arc_idx = arc_idx,
                 },
             });
         } else |err| {
@@ -373,8 +353,6 @@ const Voronoi = struct {
     /// Each event splits one arc in two
     pub fn siteEvent(self: *@This(), allocator: std.mem.Allocator, focus: std.meta.TagPayload(EventQueue.Event, .site)) !void {
         log.info("Beachline {any}", .{self.beachline});
-        const beach_len = self.beachline.arcs.items.len;
-        // Boundary Conditions
         // Locate the existing arc (if any) that is above the new site
         const idx, const yval =
             self.beachline.findOverlappingArcInfo(focus);
@@ -392,31 +370,17 @@ const Voronoi = struct {
             const start_vertex = try self.mesh.addVertex(edge_start);
             const edge = try self.mesh.rawAddEdge(start_vertex, null, {}, true);
 
-            // Dark magic tm.
-            const should_not_have_left_boundary =
-                (beach_len != 1 and old_arc.boundary_left == null) or
-                (beach_len == 1 and focus.x < old_arc.focus.x);
-            const should_not_have_right_boundary =
-                (beach_len != 1 and old_arc.boundary_right == null) or
-                (beach_len == 1 and focus.x > old_arc.focus.x);
+            const inner_boundary_left = try self.beachline.createBoundary(.{
+                .mesh_hedge = edge.half,
+                .direction = edge_direction,
+                .start = edge_start,
+            });
 
-            const inner_boundary_left = if (should_not_have_left_boundary)
-                null
-            else
-                try self.beachline.createBoundary(.{
-                    .mesh_hedge = edge.half,
-                    .direction = edge_direction,
-                    .start = edge_start,
-                });
-
-            const inner_boundary_right = if (should_not_have_right_boundary)
-                null
-            else
-                try self.beachline.createBoundary(.{
-                    .mesh_hedge = edge.half.twin,
-                    .direction = edge_direction.negate(),
-                    .start = edge_start,
-                });
+            const inner_boundary_right = try self.beachline.createBoundary(.{
+                .mesh_hedge = edge.half.twin,
+                .direction = edge_direction.negate(),
+                .start = edge_start,
+            });
 
             const new_arcs: [3]Beachline.Arc = .{
                 .{ // Left
@@ -439,18 +403,12 @@ const Voronoi = struct {
                 },
             };
 
-            const replacement_range: []const Beachline.Arc = if (should_not_have_left_boundary)
-                new_arcs[1..3]
-            else if (should_not_have_right_boundary)
-                new_arcs[0..2]
-            else
-                &new_arcs;
+            const replacement_range: []const Beachline.Arc = &new_arcs;
 
-            log.info("Replacing arc {{{d}, {d}}} at {d} with [{d}] -> {any}", .{
+            log.info("Replacing arc {{{d}, {d}}} at {d} with {any}", .{
                 focus.x,
                 focus.y,
                 idx,
-                replacement_range.len,
                 replacement_range,
             });
 
@@ -461,12 +419,8 @@ const Voronoi = struct {
                 replacement_range,
             );
 
-            if (should_not_have_right_boundary) try self.addCircleEvent(
-                idx -| 1,
-            );
-            if (should_not_have_left_boundary) try self.addCircleEvent(
-                if (idx + 1 == beach_len) idx else idx + 1,
-            );
+            try self.addCircleEvent(idx -| 1);
+            try self.addCircleEvent(idx + 1);
         } else {
             @branchHint(.cold);
             try self.beachline.arcs.append(allocator, .{
@@ -483,7 +437,7 @@ const Voronoi = struct {
     pub fn circleEvent(self: *@This(), allocator: std.mem.Allocator, inner: std.meta.TagPayload(EventQueue.Event, .circle)) !void {
         assert(inner.cancelled == false);
         assert(inner.cancelled == false);
-        const arc: *Beachline.Arc = inner.arc;
+        const arc: *Beachline.Arc = self.beachline.getArcPtr(inner.arc_idx);
         assert(arc.boundary_left != null and arc.boundary_right != null);
         const intersection_point = inner.center;
         _ = intersection_point; // autofix
@@ -493,7 +447,6 @@ const Voronoi = struct {
         // },
         log.info("Running circle event", .{});
         _ = allocator; // autofix
-        _ = self; // autofix
         // arc.boundary_left.?.mesh_hedge.();
 
         // Add the vertex to the corresponding edge in the mesh
